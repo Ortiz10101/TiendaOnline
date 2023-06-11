754332c65ad2e6c3e3445cc1f5ff341044dd6808
Rogerslug <rogelio.020496@gmail.com>
2023-06-11 01:17:19 -0600

    Acomodo de puerto de 5000 a 3000

--- servidor/server.js
+++ servidor/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log(`Servidor corriendo en el puerto ${port}`);
